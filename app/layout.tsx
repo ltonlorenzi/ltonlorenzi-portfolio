aa9a3cd0ebe39f8bed7b151f753e782e326ae329
Luciano <luciano.tonlorenzi@gmail.com>
2024-10-14 18:37:58 -0300

    work in progress...

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <div className="mb-2">LTONLORENZI PORTFOLIO</div>
-          <div>Coming soon...</div>
-        </div>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        I&apos;m a Full Stack Web Developer specializing in MERN technologies.
+      </p>
+      <p>Explore my projects below!</p>
+    </main>
   );
 }
